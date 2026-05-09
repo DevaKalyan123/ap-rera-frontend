99e9b3f54a16155a4dabab5b5c92f7b3afe00d49
DevaKalyan123 <devakalyaneepi@gmail.com>
2026-05-09 09:50:40 +0530

    fixed existing project css filename

--- src/pages/projectDetails.jsx
+++ src/pages/projectDetails.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate,useLocation } from "react-router-dom";
 
-
-
 import "../styles/ProjectDetails.css";
 import ProjectWizard from "../components/ProjectWizard";
 
